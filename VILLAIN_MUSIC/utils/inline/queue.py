68de8fa929354a90d82180ef3ebd14893311e8e2
PROOTAKU2 <prootaku99963@gmail.com>
2025-07-24 23:47:46 +0530

    Update queue.py

--- VILLAIN_MUSIC/utils/inline/queue.py
+++ VILLAIN_MUSIC/utils/inline/queue.py
@@ -1,6 +1,5 @@
 from typing import Union
-from VILLAIN_MUSIC import app
-from VILLAIN_MUSIC.utils.formatters import time_to_seconds
+
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 
@@ -66,19 +65,12 @@ def queue_back_markup(_, CPLAY):
 
 def aq_markup(_, chat_id):
     buttons = [
-        [   
-            InlineKeyboardButton(text=_["S_B_3"], url=f"https://t.me/{app.username}?startgroup=true&admin=delete_messages+invite_users",)
-        ],
         [
-            InlineKeyboardButton(text="˹ ʀєsᴜᴍє ˼", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="˹ ᴘᴧᴜsє ˼", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
-        [
-            InlineKeyboardButton(text="˹ sᴋɪᴘ ˼", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="˹ sᴛσᴘ ˼", callback_data=f"ADMIN Stop|{chat_id}"),
-        ],
-        [
-            InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close",),
-        ]
+        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
